d4888020287292862f80adc40bd20cff80e1fd16
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-13 09:26:15 +0200

    Update print-date.js

--- print-date.js
+++ print-date.js
@@ -1,2 +1,11 @@
 // add a printDate function in here
 // eg: console.log("Hello World")
+function  currentdatetime() {
+  var today = new Date();
+  var date = today.getFullYear()+"-"+ today.getMonth() + "-" + today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var datetime = date + "" + time;
+  console.log(datetime);
+}
+  
+  
