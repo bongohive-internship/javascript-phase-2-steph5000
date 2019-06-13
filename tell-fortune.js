630f2d3b01016d9ab0a0e65d725b8dbeb0eeca50
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-13 11:34:28 +0200

    tellfortune

--- tell-fortune.js
+++ tell-fortune.js
@@ -1,1 +1,7 @@
 // tellFortune function should be here
+function tellfortune(job, location, partner, children) {
+  console.log("you are a" + "" + job + "" + "in" + "" + location +"" + "married to" + "" + partner + "" + "" "with" + ""+ children + ""+ "children"
+              }
+     tellfortune("doctor", "lusaka", "nancy", "6");
+  tellfortune("driver", "kitwe", "julie", "8");
+  tellfortune("vet", "mansa", "annie" "10");
