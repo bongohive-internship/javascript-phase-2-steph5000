1b2453e600abef7d7cc95e7712a86df72dcbd5fe
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-13 11:09:16 +0200

    circumfrence

--- calc-circumfrence.js
+++ calc-circumfrence.js
@@ -1,1 +1,7 @@
 // calcCircumfrence function should be here
+function calcCircumfrence(radius) {
+  var circumfrence = 2 * Math.PI * radius;
+  conole.log("The circumfrence is" + "" + circumfrence);
+  var area = Math.PI * radius **2;
+}
+  
